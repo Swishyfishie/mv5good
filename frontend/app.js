add78de2acb4ac3d35a916aac9c582500896cca4
Swishyfishie <pclandreiuk@gmail.com>
2020-07-24 03:37:03 +0100

    rename post

--- frontend/app.js
+++ frontend/app.js
@@ -3,6 +3,18 @@ window.addEventListener("load", function(){
 
     user.getUser()
 
-    
+    postConcert = document.querySelector("#addConcert")
+    postConcert.addEventListener("click", function(e){
+        console.log("hi")
+        e.preventDefault()
+        event = document.querySelector("#event")
+        location = document.querySelector("#location")
+        time = document.querySelector("#time")
+        date = document.querySelector("#date")
+
+        let newConcert = new Post(event,location,date,time)
+
+        newConcert.postConcert()
+    })
 })
 
