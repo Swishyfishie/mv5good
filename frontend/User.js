559efb0eb7ba48409ace20c2db28f926761edd28
Swishyfishie <pclandreiuk@gmail.com>
2020-07-27 22:48:10 +0100

    refactor render concerts

--- frontend/User.js
+++ frontend/User.js
@@ -17,26 +17,44 @@ class User {
         }
 
         function _renderConcerts(concerts){
-            let list = document.querySelector(".concertList")
-            let acceptedArgs = ['date', 'location','ticket_price','time','event','more_info']
-            let statsArr = []
+            // let list = document.querySelector(".concertList")
+            // let acceptedArgs = ['date', 'location','ticket_price','time','event','more_info']
+            // let statsArr = []
+            // concerts.map(e=>{
+            //     for(let [key, val] of Object.entries(e)){
+            //         if(acceptedArgs.includes(key)){
+            //             console.log(key.toUpperCase()+ ' ' + val)
+            //             statsArr.push(key.toUpperCase()+ ' ' + val)
+            //         }
+            //     }
+            //     console.log(statsArr)
+            //     list.innerHTML += `
+            //     <div class="concertCard">
+            //     <p>${statsArr.map(e=>e).join(' ')}</p>
+            //     </div>
+                
+            //     `
+            //     statsArr = []
+
+            // })
+            
+            // console.log(typeof concerts)
             concerts.map(e=>{
-                for(let [key, val] of Object.entries(e)){
-                    if(acceptedArgs.includes(key)){
-                        console.log(key.toUpperCase()+ ' ' + val)
-                        statsArr.push(key.toUpperCase()+ ' ' + val)
+                let list = document.querySelector(".concertList")
+                let acceptedArgs = ['date', 'location','ticket_price','time','event','more_info']
+                let eventDiv = document.createElement("DIV")
+                eventDiv.classList = "event"
+                list.appendChild(eventDiv)
+                for(let v in e){
+                    if(acceptedArgs.includes(v)){
+                        
+                        eventDiv.innerHTML += `
+                        
+                        <p>${v} : ${e[v]}</p> 
+                        `
                     }
                 }
-                list.innerHTML += `Concert
-                <div class="concertCard">
-                <p>${statsArr.map(e=>e).join(' ')}</p>
-                </div>
-                
-                `
-                statsArr = []
-
             })
-            
         }
 
         
